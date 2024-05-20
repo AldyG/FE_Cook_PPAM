30b0bed08fc10906953b7d4ee91c6223220c8068
Coding Miku <102744661+toper664@users.noreply.github.com>
2024-05-21 00:34:52 +0700

    feat: improved file management and barrelling

---
+++ screen/index.tsx
@@ -0,0 +1,17 @@
+//Barrel File for exports from screen folder.
+
+export { default as welcome } from "./welcome";
+export { default as login } from "./login";
+export { default as register } from "./register";
+export { default as home } from "./home";
+export { default as own } from "./own-recipes";
+export { default as collection } from "./collection";
+export { default as profile } from "./profile";
+// export { default as add } from "./add-recipes";
+// export { default as browse } from "./browse";
+// export { default as change } from "./change-profile";
+// export { default as payment } from "./payment";
+// export { default as recipe } from "./recipe";
+// export { default as review } from "./review";
+// export { default as upgrade } from "./upgrade";
+
