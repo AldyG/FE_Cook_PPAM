f7ec0c76c97c129fce07acc61c4ee08d41f1d9cb
Laurentia Kayleen Christopher <18221053@std.stei.itb.ac.id>
2024-06-11 17:55:26 +0700

    upgrade done

--- app/(tabs)/profiling/upgrade.tsx
+++ app/(tabs)/profiling/upgrade.tsx
@@ -1,15 +1,192 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Image, Pressable, Dimensions, TextInput } from "react-native";
+import { Link } from 'expo-router'
+import { DMSans_700Bold, DMSans_400Regular, useFonts} from "@expo-google-fonts/dm-sans";
+import { Redirect } from 'expo-router';
 
 interface prop {
   // prop halaman
 }
 
+const styles = StyleSheet.create({
+	upgradebg: {
+    backgroundColor: "#edf9ff",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
+  },
+  upgrade1: {
+		backgroundColor: "#edf9ff",
+		height: 900,
+		position: "relative",
+		width: 430,
+	},
+  box: {
+		height: 55,
+		width: 430,
+	},
+	boxrectangle: {
+		backgroundColor: "#87e4fd",
+		height: 55,
+		left: 0,
+		position: "absolute",
+		top: 0,
+		width: 430,
+	},
+  upgrade_header: {
+    fontFamily: "DMSans_700Bold",
+    fontSize: 20,
+    color: "#080738",
+    width: 250,
+    left: 70,
+    top: 11
+  },
+  backarrow: {
+    height: 20,
+    left: 35,
+    position: "absolute",
+    top: 16,
+    width: 20
+  },
+  backgroundStar: {
+    height: 188,
+    width: 371,
+    top: 100,
+    left: 31,
+    position: "absolute"
+  },
+  intersect: {
+    height: 157,
+    left: 0,
+    top: 680,
+    position: "absolute",
+    width: 430
+  },
+  benefitbox: {
+    backgroundColor: "#ffffff",
+    borderWidth: 0.5,
+    borderColor: "#87e4fd",
+    borderRadius: 13,
+    height: 140,
+    left: 44,
+    top: 240,
+    width: 345
+  },
+  benefits: {
+    color: "#080738",
+    fontFamily: "DMSans_700Bold",
+    fontSize: 18,
+    left: 6,
+    top: 2,
+    lineHeight: 24,
+    position: "absolute",
+    textAlign: "justify",
+    width: 350,
+  },
+  benefitdesc: {
+    color: "#080738",
+    fontFamily: "DMSans_400Regular",
+    fontSize: 14,
+    left: 30,
+    top: 23,
+    lineHeight: 25,
+    position: "absolute",
+    textAlign: "justify",
+    width: 350,
+  },
+  subscriptionlabel: {
+    color: "#080738",
+    fontFamily: "DMSans_700Bold",
+    fontSize: 18,
+    left: 50,
+    top: 400,
+    width: 192,
+    lineHeight: 24,
+    position: "absolute",
+  },
+  hargasubscription: {
+    color: "#51b0f4",
+    fontFamily: "DMSans_700Bold",
+    fontSize: 18,
+    left: 10,
+    top: 10,
+    lineHeight: 24,
+    position: "absolute",
+    textAlign: "justify",
+    width: 350,
+  },
+  subbox: {
+    backgroundColor: "#ffffff",
+    borderWidth: 0.5,
+    borderColor: "#87e4fd",
+    borderRadius: 13,
+    height: 50,
+    left: 44,
+    top: 315,
+    width: 345
+  },
+  dots: {
+    height: 100,
+    left: 12,
+    position: "absolute",
+    top: 30,
+    width: 10
+  },
+  happyman: {
+    height: 249,
+    left: 72,
+    objectFit: "cover",
+    position: "absolute",
+    top: 590,
+    width: 279
+  },
+  subscribebutton: {
+    height: 50,
+    width: 200,
+    backgroundColor: "#090838",
+    borderRadius: 10,
+    left: 111,
+    top: 380,
+  },
+  subscribetext: {
+    fontFamily: "DMSans_700Bold",
+    color: "#ffffff",
+    fontSize: 16,
+    left: 50,
+    top: 12,
+  }
+});
+
 const Upgrade: React.FC<prop> = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>UPGRADE RAHHHHHHHHHHHHHHHHHHHHH</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.upgradebg}>
+      <View style={styles.upgrade1}>
+        <Link style={styles.boxrectangle} href={"/(tabs)/profile"} asChild push>
+          <Pressable>
+            <Text style={styles.upgrade_header}>Upgrade your Account</Text>
+            <Image style= {styles.backarrow} source={require("../../../images/backarrow2.png")}/>
+          </Pressable>
+        </Link>
+        <Image style= {styles.backgroundStar} source={require("../../../images/star.png")}/>
+        <View style={styles.benefitbox}>
+          <Text style={styles.benefits}>Benefits</Text>
+          <Image style= {styles.dots} source={require("../../../images/dots.png")}/>
+          <Text style={styles.benefitdesc}>Save unlimited recipes!{"\n"}Access video instructions!{"\n"}No ads!{"\n"}Add unlimited recipes!</Text>
+        </View>
+        <Text style={styles.subscriptionlabel}>Subscription</Text>
+        <View style={styles.subbox}>
+          <Text style={styles.hargasubscription}>Rp 100.000,00</Text>
+        </View>
+        <Image style= {styles.intersect} source={require("../../../images/intersect.png")}/>
+        <Image style= {styles.happyman} source={require("../../../images/happyman.png")}/>
+        <Link style={styles.subscribebutton} href={"/(tabs)/profiling/payment"} asChild push>
+          <Pressable>
+            <Text style={styles.subscribetext}>SUBSCRIBE</Text>
+          </Pressable>
+        </Link>
+      </View>
+    </ScrollView>
   );
 };
 
